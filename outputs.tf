5c60528baaf0f3b7e487a16a36a1eafdf1234a0b
blueberry247 <32651184+blueberry247@users.noreply.github.com>
2026-07-01 14:04:34 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,18 @@
+####################################################
+# Outputs
+####################################################
+
 output "webapp_url" {
-  value = "https://${azurerm_linux_web_app.webapp.default_hostname}"
+  description = "Azure Web App URL"
+  value       = "https://${azurerm_linux_web_app.webapp.default_hostname}"
 }
 
-output "resource_group" {
-  value = azurerm_resource_group.rg.name
+output "resource_group_name" {
+  description = "Azure Resource Group Name"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "app_service_plan_name" {
+  description = "Azure App Service Plan Name"
+  value       = azurerm_service_plan.plan.name
 }
