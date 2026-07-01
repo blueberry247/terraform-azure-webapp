fd08dccb94311e94da59f952fd41536048987d5f
blueberry247 <32651184+blueberry247@users.noreply.github.com>
2026-07-01 14:05:37 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,21 @@
+####################################################
+# Terraform Configuration
+####################################################
+
 terraform {
   required_version = ">= 1.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "~> 4.0"
     }
   }
 }
+
+####################################################
+# Azure Provider
+####################################################
 
 provider "azurerm" {
   features {}
