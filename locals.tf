22de2b0fec3719e349670abb014f936e1dc5ca9a
blueberry247 <32651184+blueberry247@users.noreply.github.com>
2026-07-01 13:58:56 +0100

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,14 @@
+####################################################
+# Local Variables
+####################################################
+
+locals {
+
+  common_tags = {
+    Environment = "Dev"
+    Project     = "Terraform Azure Web App"
+    Owner       = "Mohammed Farooq"
+    ManagedBy   = "Terraform"
+  }
+
+}
